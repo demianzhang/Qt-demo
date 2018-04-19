eb7e3fab0bbf3c6fff4c715de356f2ea644fa81c
张建功 <zjg@demian-2.local>
2018-04-19 23:16:10 +0800

    init

---
+++ widget.cpp
@@ -0,0 +1,174 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QDateTime>
+#include <QtWidgets>
+#include <QVideoWidget>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    //playButton
+    ui->m_playButton->setEnabled(false);
+    ui->m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    connect(ui->m_playButton, &QAbstractButton::clicked,this, &Widget::play);
+
+    //openButton
+    connect(ui->openButton, &QAbstractButton::clicked, this, &Widget::openFile);
+
+    //QMediaPlayer & QVideoWidget
+    m_mediaPlayer = new QMediaPlayer(this, QMediaPlayer::VideoSurface);
+    QVideoWidget *videoWidget = new QVideoWidget;
+    QBoxLayout * layout_video = new QVBoxLayout;
+    layout_video->setMargin(1);
+    videoWidget->resize(ui->label_player->size());
+    layout_video->addWidget(videoWidget);
+    ui->label_player->setLayout(layout_video);
+    m_mediaPlayer->setVideoOutput(videoWidget);
+    //Slider
+    ui->horizontalSlider->setRange(0, 0);
+    connect(ui->horizontalSlider, &QAbstractSlider::sliderMoved,this, &Widget::setPosition);
+
+    m_errorLabel = new QLabel;
+    m_errorLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum);
+
+    maxSize = 31; // 只存储最新的 31 个数据
+    maxX = 1000;
+    maxY = 10;
+    splineSeries = new QSplineSeries();
+    scatterSeries = new QScatterSeries();
+    scatterSeries->setMarkerSize(8);
+    chart = new QChart();
+    chart->addSeries(splineSeries);
+    chart->addSeries(scatterSeries);
+    chart->legend()->hide();
+    //chart->setTitle("实时动态曲线");
+    chart->createDefaultAxes();
+    chart->axisX()->setRange(0, maxX);
+    chart->axisY()->setRange(0, maxY);
+    chart->removeAxis(chart->axisX());
+    chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    QBoxLayout *layout = new QVBoxLayout;
+    layout->setContentsMargins(0,0,0,0);
+    layout->addWidget(chartView);
+    ui->label2->setLayout(layout);
+    timerId = startTimer(200);
+    qsrand(QDateTime::currentDateTime().toTime_t());
+
+    connect(m_mediaPlayer, &QMediaPlayer::stateChanged,this, &Widget::mediaStateChanged);
+    connect(m_mediaPlayer, &QMediaPlayer::positionChanged, this, &Widget::positionChanged);
+    connect(m_mediaPlayer, &QMediaPlayer::durationChanged, this, &Widget::durationChanged);
+    connect(m_mediaPlayer, QOverload<QMediaPlayer::Error>::of(&QMediaPlayer::error),this, &Widget::handleError);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::openFile()
+{
+    QFileDialog fileDialog(this);
+    fileDialog.setAcceptMode(QFileDialog::AcceptOpen);
+    fileDialog.setWindowTitle(tr("Open Movie"));
+    QStringList supportedMimeTypes = m_mediaPlayer->supportedMimeTypes();
+    if (!supportedMimeTypes.isEmpty())
+        fileDialog.setMimeTypeFilters(supportedMimeTypes);
+    fileDialog.setDirectory(QStandardPaths::standardLocations(QStandardPaths::MoviesLocation).value(0, QDir::homePath()));
+    if (fileDialog.exec() == QDialog::Accepted)
+        setUrl(fileDialog.selectedUrls().constFirst());
+}
+
+void Widget::setUrl(const QUrl &url)
+{
+    m_errorLabel->setText(QString());
+    setWindowFilePath(url.isLocalFile() ? url.toLocalFile() : QString());
+    m_mediaPlayer->setMedia(url);
+    ui->m_playButton->setEnabled(true);
+}
+
+void Widget::play()
+{
+    switch (m_mediaPlayer->state()) {
+    case QMediaPlayer::PlayingState:
+        m_mediaPlayer->pause();
+        break;
+    default:
+        m_mediaPlayer->play();
+        break;
+    }
+}
+
+void Widget::mediaStateChanged(QMediaPlayer::State state)
+{
+    switch(state) {
+    case QMediaPlayer::PlayingState:
+        ui->m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+        break;
+    default:
+        ui->m_playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+        break;
+    }
+}
+
+void Widget::positionChanged(qint64 position)
+{
+    ui->horizontalSlider->setValue(position);
+}
+
+void Widget::durationChanged(qint64 duration)
+{
+    ui->horizontalSlider->setRange(0, duration);
+}
+
+void Widget::setPosition(int position)
+{
+    m_mediaPlayer->setPosition(position);
+}
+
+void Widget::handleError()
+{
+    ui->m_playButton->setEnabled(false);
+    const QString errorString = m_mediaPlayer->errorString();
+    QString message = "Error: ";
+    if (errorString.isEmpty())
+        message += " #" + QString::number(int(m_mediaPlayer->error()));
+    else
+        message += errorString;
+    m_errorLabel->setText(message);
+}
+
+
+void Widget::timerEvent(QTimerEvent *event) {
+    // 产生一个数据，模拟不停的接收到新数据
+    if (event->timerId() == timerId) {
+        int newData = qrand() % (maxY + 1);
+        dataReceived(newData);
+    }
+}
+
+
+void Widget::dataReceived(int value) {
+    data << value;
+    // 数据个数超过了最大数量，则删除最先接收到的数据，实现曲线向前移动
+    while (data.size() > maxSize) {
+        data.removeFirst();
+    }
+    // 界面被隐藏后就没有必要绘制数据的曲线了
+    if (isVisible()) {
+        splineSeries->clear();
+        scatterSeries->clear();
+        int dx = maxX / (maxSize-1);
+        int less = maxSize - data.size();
+        for (int i = 0; i < data.size(); ++i) {
+            splineSeries->append(less*dx+i*dx, data.at(i));
+            scatterSeries->append(less*dx+i*dx, data.at(i));
+        }
+    }
+}
---
+++ widget.h
@@ -0,0 +1,72 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QMediaPlayer>
+#include <QWidget>
+#include <QList>
+
+#include <QSplineSeries>
+#include <QScatterSeries>
+#include <QChart>
+#include <QChartView>
+
+
+namespace Ui {
+class Widget;
+}
+using namespace QtCharts;
+
+QT_BEGIN_NAMESPACE
+class QAbstractButton;
+class QSlider;
+class QLabel;
+class QUrl;
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+    void setUrl(const QUrl &url);
+
+public slots:
+    void openFile();
+    void play();
+
+private slots:
+    void mediaStateChanged(QMediaPlayer::State state);
+    void positionChanged(qint64 position);
+    void durationChanged(qint64 duration);
+    void setPosition(int position);
+    void handleError();
+
+private:
+    QMediaPlayer* m_mediaPlayer;
+    QAbstractButton *m_playButton;
+    QSlider *m_positionSlider;
+    QLabel *m_errorLabel;
+
+    Ui::Widget *ui;
+
+    /**
+     * 接收到数据源发送来的数据，数据源可以下位机，采集卡，传感器等。
+     */
+    void dataReceived(int value);
+    int timerId;
+    int maxSize;  // data 最多存储 maxSize 个元素
+    int maxX;
+    int maxY;
+    QList<double> data; // 存储业务数据的 list
+    QChart *chart;
+    QChartView *chartView;
+    QSplineSeries *splineSeries;
+    QScatterSeries *scatterSeries;
+
+protected:
+    void timerEvent(QTimerEvent *event) Q_DECL_OVERRIDE;
+
+};
+#endif // WIDGET_H
